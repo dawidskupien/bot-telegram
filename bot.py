35fd88d609c70e512403d62cdb79f602ff564aa2
Dawid <112086896+dawidskupien@users.noreply.github.com>
2025-04-02 01:50:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import os
 from twilio.rest import Client
 from telethon import TelegramClient, events
 
-
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 phone_number = os.getenv("PHONE_NUMBER")
@@ -14,28 +13,23 @@ auth_token = os.getenv("AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
 
-# Inicjalizacja klienta Telethon
 Tclient = TelegramClient("session", api_id, api_hash)
 
 def make_call():
     print("📞 Dzwonię do ciebie...")
-
-    # Wywołanie połączenia na numer telefonu (Twój numer)
+    
     call = client.calls.create(
-        from_=twilio_phone_number,  # Twój numer Twilio
-        to=phone_number,  # Twój numer telefonu
+        from_=twilio_phone_number, 
+        to=phone_number,
         url="http://demo.twilio.com/docs/voice.xml"  # Adres do dźwięku połączenia
     )
-
     print(f"Połączenie SID: {call.sid}")
-
 
 # Logowanie (tylko przy pierwszym uruchomieniu trzeba wpisać kod z SMS)
 async def main():
     await Tclient.start(phone_number)
     print("✅ Bot jest gotowy!")
 
-# Nasłuchiwanie wiadomości w grupach
 @Tclient.on(events.NewMessage(chats=None))  # None = wszystkie czaty, można dodać ID grupy
 async def auto_reply(event):
     if "oddam" in event.message.text.lower():
@@ -43,7 +37,6 @@ async def auto_reply(event):
         await event.reply("Wezmę")
         make_call()
 
-# Uruchomienie bota
 with Tclient:
     Tclient.loop.run_until_complete(main())
     Tclient.run_until_disconnected()
